8f289f27c366dbb6d052f6aef2c8f9ab57b7975e
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-03-08 21:45:14 +0000

    Configure the .env file to be read from within the backend folder

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,10 +2,10 @@
 import cors from "cors"
 import dotenv from "dotenv"
 import express from "express"
-import { router as adminRouter } from "./routes/adminRoutes";
-import { router as userRouter } from "./routes/userRoutes";
+import { router as adminRouter } from "./routes/adminRoutes.js";
+import { router as userRouter } from "./routes/userRoutes.js";
 
-dotenv.config(); // loads in env variables - to be used via the process object
+dotenv.config({ path: './backend/.env' }); // loads in env variables - to be used via the process object
 const app = express();
 app.use(cors())
 app.use("/api/users", userRouter);
@@ -13,9 +13,10 @@ app.use("/api/admin", adminRouter);
 
 
 
-app.use(express.json())
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+console.log(process.env.PORT)
 
 app.get("/", (req, res) => {
     res.json({message: "Hello World",
--- backend/database/database.js
+++ backend/database/database.js
@@ -1,1 +1,4 @@
 import mssql from "mssql"
+import dotenv from "dotenv"
+
+dotenv.config({ path: './backend/.env' }); // loads in env variables - to be used via the process object
