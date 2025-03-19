198aca29c7c56ce5497165a1fdf987147cc9c4ce
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-03-19 05:08:05 +0000

    Create action button component

--- src/components/ActionButtonModule/ActionButton.jsx
+++ src/components/ActionButtonModule/ActionButton.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './ActionButton.module.css'
 
-function ActionButton() {
+
+function ActionButton({ title, action }) {
+  
+  const handleOnClick = (fn) => {
+    if( typeof fn == "function" && fn){
+      fn()
+    }
+  }
+
   return (
-    <div>ActionButton</div>
+    <button className={styles.actionButton} onClick={() => {handleOnClick(action)}}>{title}</button>
   )
 }
+
+ActionButton.propTypes = {
+  title: PropTypes.string.isRequired,  // title is a required string
+  action: PropTypes.function
+};
 
 export default ActionButton
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import ActionButton from '../../components/ActionButtonModule/ActionButton'
 import styles from './Home.module.css'
 
 function Home() {
@@ -7,15 +8,16 @@ function Home() {
   }
 
   return (
-    <div className={styles.homeBody}>
-
+    <>
       <h1 className={styles.pageTitle}>Home</h1>
       <div>
         gipvvjefvpievjpievjip
       </div>    
-      <button onClick={startExternalLogin}>Login</button>
-
-    </div>
+      <ActionButton 
+        title="Log In"
+        action={startExternalLogin}
+      />
+    </>
   )
 }
 
