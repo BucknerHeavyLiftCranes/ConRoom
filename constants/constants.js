bdf4e9dacd688270bc5ee29805d5d4e6efc9bab0
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-03-20 00:37:23 +0000

    Create database commands object for consistency

--- constants/constants.js
+++ constants/constants.js
@@ -1,7 +1,7 @@
 /**
  * Enum of various HTTP status codes for error checking.
  */
-export const httpStatusCodes = {
+export const HTTP_STATUS_CODES = {
     VALIDATION_ERROR: 400,
     UNAUTHORIZED: 401,
     FORBIDDEN: 403,
@@ -10,4 +10,37 @@ export const httpStatusCodes = {
     SERVER_ERROR: 500
 }
 
-export const dbCommands = {}
+const STATUS_MAP = {
+    1: "Scheduled",
+    2: "Ongoing",
+    3: "Completed",
+    4: "Cancelled",
+}
+
+export const DB_COMMANDS = {
+    createBucknerConroomDatabase: `
+        IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Buckner_Conroom')
+        BEGIN
+            CREATE DATABASE [Buckner_Conroom];
+        END
+    `,
+    
+    createUsersTable: `
+        IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'users')
+        BEGIN
+            CREATE TABLE users (
+                user_id INT IDENTITY(1,1) PRIMARY KEY,
+                name VARCHAR(255),
+                email VARCHAR(255) UNIQUE
+            );
+        END;
+    `,
+
+    createRoomsTable: `
+    CREATE TABLE IF NOT EXISTS rooms ()
+    `,
+
+    createReservationsTable: `
+    CREATE TABLE IF NOT EXISTS reservations ()
+    `
+}
