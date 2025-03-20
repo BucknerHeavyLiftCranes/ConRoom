120f45b8c8afa8e2c2f1aa890c719b6624ae7320
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-03-20 00:48:23 +0000

    Create functions to handle full database creation and setup

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,4 +1,47 @@
-import mssql from "mssql"
-import dotenv from "dotenv"
+import { connectToDatabase } from "../config/dbConnection.js"
+import { DB_COMMANDS } from "../../constants/constants.js"
 
-dotenv.config({ path: './backend/.env' }); // loads in env variables - to be used via the process object
+
+let pool
+
+try{
+    pool = await connectToDatabase()
+}catch (err) {
+    console.log(`DATABASE ERROR: ${err}`)
+}
+
+async function createBucknerConroomDatabase() {
+    try {
+      // Create a request and execute the SQL command stored in DB_COMMANDS.createUsersTable
+      await pool.request().query(DB_COMMANDS.createBucknerConroomDatabase);
+      console.log("Buckner_Conroom Database created successfully.");
+    } catch (err) {
+      throw new Error(`BUCKNER DATABASE CREATION ERROR: ${err}`)
+    }
+}
+
+
+
+async function createUsersTable() {
+    try {
+      // Create a request and execute the SQL command stored in DB_COMMANDS.createUsersTable
+      await pool.request().query(DB_COMMANDS.createUsersTable);
+      console.log("User table created successfully.");
+    } catch (err) {
+      throw new Error(`USER TABLE CREATION ERROR: ${err}`)
+    }
+}
+
+export async function databaseSetup(){
+    try {
+        createBucknerConroomDatabase()
+        createUsersTable()
+    } catch (err) {
+        console.log(err)   
+    }
+}
+
+
+
+
+// dotenv.config({ path: './backend/.env' }); // loads in env variables - to be used via the process object
