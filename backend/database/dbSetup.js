be80a1df5e4f904525722ba3101e837e9d41618e
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-03-21 03:14:00 +0000

    Move rooms table CRUD operation to a different file for better readability and abstraction.

---
+++ backend/database/dbSetup.js
@@ -0,0 +1,88 @@
+import { connectToDatabase } from "../config/dbConnection.js"
+import { DB_COMMANDS } from "../../constants/dbCommands.js"
+
+
+let pool
+
+try{
+    pool = await connectToDatabase()
+}catch (err) {
+    console.log(`DATABASE ERROR: ${err}`)
+}
+
+/**
+ * Create application database.
+ */
+async function createBucknerConroomDatabase() {
+    try {
+      // Create a request and execute the SQL command stored in DB_COMMANDS.createUsersTable
+      await pool.request().query(DB_COMMANDS.createBucknerConroomDatabase);
+    } catch (err) {
+      throw new Error(`BUCKNER DATABASE CREATION ERROR: ${err}`)
+    }
+}
+
+/**
+ * Create Users table in the database.
+ */
+async function createUsersTable() {
+    try {
+      // Create a request and execute the SQL command stored in DB_COMMANDS.createUsersTable
+      await pool.request().query(DB_COMMANDS.createUsersTable);
+    } catch (err) {
+      throw new Error(`USERS TABLE CREATION ERROR: ${err}`)
+    }
+}
+
+/**
+ * Create Rooms table in the database.
+ */
+async function createRoomsTable() {
+    try {
+        // Create a request and execute the SQL command stored in DB_COMMANDS.createRoomsTable
+        await pool.request().query(DB_COMMANDS.createRoomsTable);
+    } catch (err) {
+      throw new Error(`ROOMS TABLE CREATION ERROR: ${err}`)
+    }
+}
+
+/**
+ * Create Reservations table in the database (with the appropriate indexes).
+ */
+async function createReservationsTable() {
+    try {
+      // Create a request and execute the SQL command stored in DB_COMMANDS.createReservationsTable
+        await pool.request().query(DB_COMMANDS.createReservationsTable);
+        await pool.request().query(DB_COMMANDS.createIndexReservationsDate);
+        await pool.request().query(DB_COMMANDS.createIndexReservationsRoomTime);
+        await pool.request().query(DB_COMMANDS.createIndexReservationsUser);
+        await pool.request().query(DB_COMMANDS.createIndexReservationsStatus);
+    } catch (err) {
+      throw new Error(`RESERVATIONS TABLE CREATION ERROR: ${err}`)
+    }
+}
+
+/**
+ * Initialize the database and setup all the tables.
+ */
+export async function setupDatabase(){
+    try {
+        createBucknerConroomDatabase()
+        createUsersTable()
+        createRoomsTable()
+        createReservationsTable()
+
+        /* Delete Tables (for debugging) */
+        // await pool.request().query(DB_COMMANDS.dropReservationsTable);
+        // await pool.request().query(DB_COMMANDS.dropUsersTable);
+        // await pool.request().query(DB_COMMANDS.dropRoomsTable);
+        console.log("Database setup completed!")
+    } catch (err) {
+        console.log(err)   
+    }
+}
+
+
+
+
+// dotenv.config({ path: './backend/.env' }); // loads in env variables - to be used via the process object
