afbab6dd3c0e69f0f85f23db003b7852982c6017
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-03-22 22:51:24 +0000

    Fix Import Error

--- errors/InvalidTimeError.js
+++ errors/InvalidTimeError.js
@@ -1,7 +1,7 @@
 /**
- * Thrown if an objects start or end time is not valid.
+ * Thrown if an object's start or end time is not valid.
  */
-export class InvalidTimeError extends Error {
+export default class InvalidTimeError extends Error {
     constructor(message) {
         super(message)
         this.name = "InvalidTimeError"
