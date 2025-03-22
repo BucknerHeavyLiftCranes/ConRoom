008e1d9a77b1443c402f4e610d49c7c7e6ccf7bc
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-03-22 03:00:42 +0000

    Test rooms table CRUD operations

--- backend/api/server.js
+++ backend/api/server.js
@@ -5,39 +5,104 @@ import express from "express"
 import { router as adminRouter } from "./routes/adminRoutes.js";
 import { router as userRouter } from "./routes/userRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
-import { setupDatabase } from "../database/dbSetup.js";
-import { getAllRooms, updateRoom } from "../database/roomsTable.js";
+import { clearDatabase, setupDatabase } from "../database/dbSetup.js";
+import { getAllRooms, createRoom, updateRoom, deleteRoom, getRoomByNameAndEmail } from "../database/roomsTable.js";
 import { Room } from "../model/Room.js";
 
 // dotenv.config({ path: './backend/.env' }); // loads in env variables - to be used via the process object // container already loads env vars so this is not needed
 
 try {
     await setupDatabase()
+    // await clearDatabase()
 } catch (err) {
     console.error({ message: err.message, stack: err.stack });
 }
 
-try {
-    const roomToUpdate = new Room(
-        2,
-        'Scrum Room B',
-        'Main Building',
-        102,
-        12,
-        0, 
-        'A small, quiet conference room made for brief yet important meetings.',
-        '09:00:00',
-        '15:00:00'
-    )
+
+// try {
+//     const roomToCheck = new Room(
+//         undefined,
+//         'Linhawks',
+//         'LinhawksRoom@bucknerheavylift.com',
+//         500,
+//         1, 
+//         'Large hall for conferences, equipped with projectors, full speaker system, and dual monitor podium setup',
+//         '08:00:00',
+//         '20:00:00'
+//     )
     
-    await updateRoom(roomToUpdate.roomId, roomToUpdate)
-} catch (err) {
-    console.error({ message: err.message, stack: err.stack });
-}
+//     const room = await getRoomByNameAndEmail(roomToCheck.roomName, roomToCheck.roomEmail)
+//     if(!room){
+//         console.log("There is no room in the database with this name and email.")
+//     }else{
+//         console.log(room)
+//     }
+// } catch (err) {
+//     console.error({ message: err.message, stack: err.stack });
+// }
+
+
+// try {
+//     const newRoom = new Room(
+//         undefined,
+//         'Twinnings',
+//         'TwinningsRoom@bucknerheavylift.com',
+//         2,
+//         0, 
+//         'Small room designed for pair scrum meetings (includes dual monitors)',
+//         '10:00:00',
+//         '15:00:00'
+//     )
+    
+//     await createRoom(newRoom)
+// } catch (err) {
+//     console.error({ message: err.message, stack: err.stack });
+// }
+
+
+// try {
+//     const roomToUpdate = new Room(
+//         3,
+//         'Mindscape',
+//         'MindscapeRoom@bucknerheavylift.com',
+//         1000,
+//         1, 
+//         'Large hall for conferences, equipped with projectors, full speaker system, and dual monitor podium setup',
+//         '08:00:00',
+//         '20:00:00'
+//     )
+    
+//     await updateRoom(roomToUpdate.roomId, roomToUpdate)
+// } catch (err) {
+//     console.error({ message: err.message, stack: err.stack });
+// }
+
+
+// try {
+//     const roomToDelete = new Room(
+//         2,
+//         'Linhawks',
+//         'LinhawksRoom@bucknerheavylift.com',
+//         1000,
+//         1, 
+//         'Large hall for conferences, equipped with projectors, full speaker system, and dual monitor podium setup',
+//         '09:00:00',
+//         '22:00:00'
+//     )
+    
+//     await deleteRoom(roomToDelete.roomId)
+// } catch (err) {
+//     console.error({ message: err.message, stack: err.stack });
+// }
 
 
 try {
-    console.log(await getAllRooms())
+    const allRooms = await getAllRooms()
+    if(allRooms.length !== 0){
+        console.log(allRooms)
+    }else{
+        console.log("There are no rooms in this database")
+    }
 } catch (err) {
     console.error({ message: err.message, stack: err.stack });
 }
