3fc8cbea263859211b2bf598513b902734b72d7d
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-03-22 05:18:32 +0000

    Implement GET operation for reservations

--- backend/database/reservationsTable.js
+++ backend/database/reservationsTable.js
@@ -1,7 +1,7 @@
 import mssql from "mssql"
 import { connectToDatabase } from "../config/dbConnection.js"
 import { DB_COMMANDS } from "../../constants/dbCommands.js"
-import { Reservation } from "../model/Reservation";
+import { Reservation } from "../model/Reservation.js";
 
 let pool
 
@@ -12,3 +12,20 @@ try{
 }catch (err) {
     console.log(`Failed to connect to database: ${err}`)
 }
+
+/**
+ * Fetch all reservations from the database.
+ * @returns {Promise<Reservation[]>} an array of all reservations in the database.
+ */
+export const getAllReservations = async () => { 
+    try {
+        const result = await pool.request().query(DB_COMMANDS.getAllReservations);
+        const allReservationsData = result.recordset;
+        const allReservations = []
+        allReservationsData.forEach((reservation) => allReservations.push(Reservation.toModel(reservation))) // convert each database record to a Reservation object
+        return allReservations
+      } catch (err) {
+          console.error({ message: err.message, stack: err.stack });
+          throw new Error(`Failed to retrieve reservations: ${err.message}`);
+      }
+ };
