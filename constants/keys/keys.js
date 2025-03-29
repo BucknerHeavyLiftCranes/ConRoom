7702d47895b337274f43403b3e590f9747015153
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-03-29 21:35:39 +0000

    add api keys for reservation and room routes

--- constants/keys/keys.js
+++ constants/keys/keys.js
@@ -2,7 +2,7 @@
 export const rootKey = `http://127.0.0.1:5173`
 
 /** @type {string} path to server root (should use more specific paths). */
-export const reservationKey = `http://127.0.0.1:5173/reservations`
+export const reservationKey = `http://127.0.0.1:5173/api/reservations`
 
 /** @type {string} path to server root (should use more specific paths). */
-export const roomKey = `http://127.0.0.1:5173/rooms`
+export const roomKey = `http://127.0.0.1:5173/api/rooms`
