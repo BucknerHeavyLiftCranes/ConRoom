4ee3de3dcbef3ceb2b3b111dac117530fe8dbd45
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-04-02 03:06:29 +0000

    abstract sso login logic to another file

--- constants/keys/keys.js
+++ constants/keys/keys.js
@@ -6,7 +6,3 @@ export const reservationKey = `http://127.0.0.1:5173/api/reservations`
 
 /** @type {string} path to server root (should use more specific paths). */
 export const roomKey = `http://127.0.0.1:5173/api/rooms`
-
-// export const authorizationURL = `https://oauth2.example.com/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&state=${process.env.STATE}`;
-// const tokenUrl = `https://login.microsoftonline.com/${process.env.CLIENT_ID}/oauth2/v2.0/token`;
-// window.location.href = authorizationURL;
