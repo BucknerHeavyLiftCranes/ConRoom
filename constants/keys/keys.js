16adf154699ae1a60d70668577c2ed61dd1bcd37
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-04-08 14:46:58 +0000

    create authKey variable for frontend authorizations requests

--- constants/keys/keys.js
+++ constants/keys/keys.js
@@ -1,8 +1,11 @@
 /** @type {string} path to server root (should use more specific paths). */
 export const rootKey = `http://127.0.0.1:5173`
 
-/** @type {string} path to server root (should use more specific paths). */
+/** @type {string} path to resevations API. */
 export const reservationKey = `http://127.0.0.1:5173/api/reservations`
 
-/** @type {string} path to server root (should use more specific paths). */
+/** @type {string} path to server rooms API. */
 export const roomKey = `http://127.0.0.1:5173/api/rooms`
+
+/** @type {string} path to server authorization API. */
+export const authKey = `http://127.0.0.1:5173//api/auth`
