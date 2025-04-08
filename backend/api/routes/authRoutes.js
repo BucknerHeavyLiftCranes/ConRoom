810452cb53b348237e33dd4dfa2ae895b8275429
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-04-08 22:15:03 +0000

    Create routes for login flow

--- backend/api/routes/authRoutes.js
+++ backend/api/routes/authRoutes.js
@@ -1,3 +1,10 @@
 import express from "express"
+import { loginFlow, authFlow, getAccessToken } from "../controllers/authController.js"
 
 export const router = express.Router()
+
+router.get("/login", loginFlow)
+
+router.get("/", authFlow)
+
+router.get('/token', getAccessToken)
