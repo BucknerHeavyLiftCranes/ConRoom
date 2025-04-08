e2b23224ce9c7eb43bec419d17331b829f8fe764
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-04-08 22:13:30 +0000

    Setup entrypoint for external login

--- src/pages/auth/LoginModule/Login.jsx
+++ src/pages/auth/LoginModule/Login.jsx
@@ -1,12 +1,18 @@
+import { authKey } from '../../../../constants/keys/keys.js'
 import ActionButton from '../../../components/ActionButtonModule/ActionButton.jsx'
-import { loginRedirect } from '../../../services/authService.js'
+import { checkResponse, loginRedirect } from '../../../services/authService.js'
 import styles from './Login.module.css'
 
 function Login() {
     
     const startExternalLogin = async () => {
         try {
-            loginRedirect()
+            // loginRedirect()
+            // const response = await fetch(`${authKey}/login`)
+            // checkResponse(response, `Failed to log in: ${response.statusText}`)
+            // console.log(await response.json())
+            window.location.href = `${authKey}/login`;
+
         } catch (err) {
             console.error({message: err.message, stack: err.stack})
             
