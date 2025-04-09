721e24d7bfe84bc280a8a25d464431b2adde3024
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-04-09 22:43:19 +0000

    Add comments

--- constants/constants.js
+++ constants/constants.js
@@ -3,8 +3,8 @@
  */
 export const HTTP_STATUS_CODES = {
     VALIDATION_ERROR: 400,
-    UNAUTHORIZED: 401,
-    FORBIDDEN: 403,
+    UNAUTHORIZED: 401, // User does not have credentials to access resource (i.e. someone not logged in).
+    FORBIDDEN: 403, // User has credentials but lacks permission to access reosurce (i.e. a user who is not an admin).
     NOT_FOUND: 404,
     REQUEST_CONFLICT: 409,
     SERVER_ERROR: 500
