cb133dd1b77cd4889721e1b37a49304d994da679
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-04-12 06:32:03 +0000

    Add admin key

--- constants/keys/keys.js
+++ constants/keys/keys.js
@@ -7,5 +7,8 @@ export const reservationKey = `http://localhost:5173/api/reservations`
 /** @type {string} path to server rooms API. */
 export const roomKey = `http://localhost:5173/api/rooms`
 
-/** @type {string} path to server authorization API. */
+/** @type {string} path to server authentication API. */
 export const authKey = `http://localhost:5173/api/auth`
+
+/** @type {string} path to server administrator API. */
+export const adminKey = `http://localhost:5173/api/admin`
