7410e953853b25705b15475962384bba5e33c704
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-04-16 15:23:43 +0000

    Implement shape of EventRequest and StaticEventRequest

--- src/models/EventRequest.js
+++ src/models/EventRequest.js
@@ -1,21 +1,42 @@
 /**
+ * @typedef {Object} emailAddress
+ * @property {string} name - Display name of the email sender or recipient.
+ * @property {string} address - Actual email address (e.g., user@abc.com).
+ * @property {string} type 
+ */
+
+/**
  * @typedef {Object} dateTimeAndZone
  * @property {string} dateTime - event date and time (in ISO format).
  * @property {string} timezone - Event timezone (i.e. `UTC`).
+ */
+
+/**
+ * @typedef {Object} location
+ * @property {string} displayName - Display name of the location (online or in-person).
  */
 
 export class EventRequest {
     constructor(
         /** @type {string} */
         subject,
+        // /** @type {emailAddress} */
+        // organizer,
          /** @type {dateTimeAndZone} */
         start,
          /** @type {dateTimeAndZone} */
-        end
+        end,
+        /** @type {location} */
+        location = {},
+        /** @type {emailAddress[]} */
+        attendees = []
+
     ) {
         this.subject = subject
         this.start = start
         this.end = end
+        this.location = location
+        this.attendees = attendees
     }
 
     /**
--- src/models/StaticEventRequest.js
+++ src/models/StaticEventRequest.js
@@ -1,17 +1,41 @@
 import { EventRequest } from "./EventRequest";
 
+/**
+ * @typedef {Object} emailAddress
+ * @property {string} name - Display name of the email sender or recipient.
+ * @property {string} address - Actual email address (e.g., user@abc.com).
+ * @property {string} type 
+ */
+
+/**
+ * @typedef {Object} dateTimeAndZone
+ * @property {string} dateTime - event date and time (in ISO format).
+ * @property {string} timezone - Event timezone (i.e. `UTC`).
+ */
+
+/**
+ * @typedef {Object} location
+ * @property {string} displayName - Display name of the location (online or in-person).
+ */
+
 export class StaticEventRequest extends EventRequest{
     constructor(
         /** @type {string} */
         subject, 
+        // /** @type {emailAddress} */
+        // organizer,
         /** @type {string} */
         start, 
+        /** @type {location} */
+        location = {},
+        /** @type {emailAddress[]} */
+        attendees = [],
         /** @type {number} */
         meetingLength){
             
         const end = StaticEventRequest.calculateEndTime(start, meetingLength)
         
-        super(subject, start, end)
+        super(subject, start, end, location, attendees)
     }
 
     /**
