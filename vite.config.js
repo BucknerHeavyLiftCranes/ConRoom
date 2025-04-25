83653cdabea4b57cc22cca98c9588a6039cd5241
TJOKOLI17 <111609074+TJOKOLI17@users.noreply.github.com>
2025-04-25 18:57:38 +0000

    Expose Vite server to public server for iPad testing

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true // or '0.0.0.0'
+    host: true, // or '0.0.0.0'
+    port: 5174,
+    allowedHosts: true
   },
   envPrefix: 'VITE_',
 })
